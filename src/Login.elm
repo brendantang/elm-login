9ca99e9d69814803403f257d23d49206a4b01f28
Brendan Tang <b@brendantang.net>
2022-08-24 11:48:09 -0400

    Initial commit

---
+++ src/Login.elm
@@ -0,0 +1,219 @@
+port module Login exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import Http
+import Json.Decode as JD
+import Json.Encode as JE
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { email : String
+    , password : String
+    , status : Status
+    , apiBaseUrl : String
+    }
+
+
+type Status
+    = Ready
+    | Authenticating
+    | Failure
+    | Problem String
+    | LoggedIn AuthToken
+
+
+type AuthToken
+    = TokenString String
+
+
+tokenToString : AuthToken -> String
+tokenToString token =
+    case token of
+        TokenString str ->
+            str
+
+
+authTokenDecoder : JD.Decoder AuthToken
+authTokenDecoder =
+    JD.map TokenString (JD.field "access_token" JD.string)
+
+
+authHeader : AuthToken -> Http.Header
+authHeader token =
+    case token of
+        TokenString str ->
+            Http.header "X-WWW-AUTH-TOKEN" str
+
+
+init : String -> ( Model, Cmd Msg )
+init apiBaseUrl =
+    ( Model "" "" Ready apiBaseUrl, fetchAuthToken () )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Email String
+    | Password String
+    | SignIn
+    | GotAuthFromServer (Result Http.Error AuthToken)
+    | GotAuthFromCookie (Maybe String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Email email ->
+            ( { model | email = email, status = Ready }, Cmd.none )
+
+        Password password ->
+            ( { model | password = password, status = Ready }, Cmd.none )
+
+        SignIn ->
+            ( { model | status = Authenticating }, getAuth model )
+
+        GotAuthFromServer result ->
+            case result of
+                Ok token ->
+                    ( { model | status = LoggedIn token }, setAuthTokenCookie (Api.tokenToString token) )
+
+                Err e ->
+                    case e of
+                        Http.BadStatus 401 ->
+                            ( { model | status = Failure }, Cmd.none )
+
+                        _ ->
+                            ( { model | status = Problem (StringUtil.prettyHttpError e) }, Cmd.none )
+
+        GotAuthFromCookie maybeTokenString ->
+            case maybeTokenString of
+                Just token ->
+                    ( { model | status = LoggedIn (Api.TokenString token) }, setAuthTokenCookie token )
+
+                Nothing ->
+                    ( { model | status = Ready }, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    case model.status of
+        LoggedIn _ ->
+            text ""
+
+        _ ->
+            UI.wrapper
+                [ viewStatus model.status
+                , viewSignInForm model
+                ]
+
+
+viewSignInForm : Model -> Html Msg
+viewSignInForm model =
+    div [ css [ displayFlex, flexDirection column ] ]
+        [ viewInput "text" "Email" model.email Email
+        , viewInput "password" "Password" model.password Password
+        , viewSignInButton model.status
+        ]
+
+
+viewSignInButton : Status -> Html Msg
+viewSignInButton status =
+    case status of
+        Ready ->
+            UI.blueButton [] SignIn "Sign in"
+
+        _ ->
+            UI.disabledButton [] "Sign in"
+
+
+viewStatus : Status -> Html Msg
+viewStatus status =
+    case status of
+        Ready ->
+            text ""
+
+        Problem description ->
+            text description
+
+        LoggedIn _ ->
+            text "Logged in!"
+
+        Authenticating ->
+            text ""
+
+        Failure ->
+            text "Incorrect email or password, try again."
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+    UI.input_ [ type_ t, placeholder p, value v, onInput toMsg, css [ Css.padding (Css.rem 1) ] ] []
+
+
+
+-- HTTP
+
+
+getAuth : Model -> Cmd Msg
+getAuth model =
+    Http.post
+        { url = model.apiBaseUrl ++ "login.json"
+        , body = Http.jsonBody (encodeCredentials model.email model.password)
+        , expect = Http.expectJson GotAuthFromServer authTokenDecoder
+        }
+
+
+encodeCredentials : String -> String -> JE.Value
+encodeCredentials email password =
+    JE.object
+        [ ( "email", JE.string email )
+        , ( "password", JE.string password )
+        ]
+
+
+
+-- PORTS
+
+
+port fetchAuthToken : () -> Cmd msg
+
+
+port authTokenReceiver : (Maybe String -> msg) -> Sub msg
+
+
+port setAuthTokenCookie : String -> Cmd msg
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    authTokenReceiver GotAuthFromCookie
